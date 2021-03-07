c15cb64f622c9862654c989257ae54cb01bb7241
liumangtu <1312483228@qq.com>
2021-03-07 09:41:04 +0800

    完成了主页功能的开发

---
+++ assets/js/index.js
@@ -0,0 +1,60 @@
+$(function() {
+    //调用 getUserInfo 函数获取用户信息
+    getUserInfo()
+    var layer = layui.layer
+        //点击按钮，实现退出功能
+    $('#btnLogout').on('click', function() {
+        //提示用户是否确定退出
+        layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, function(index) {
+            //do something
+            //1、清空本地存储中的 token 
+            localStorage.removeItem('token')
+                //2、重新跳转到登录页面
+            location.href = '/login.html'
+            layer.close(index);
+        });
+    })
+})
+
+function getUserInfo() {
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        //headers 就是请求头配置对象
+        /* headers: {
+            Authorization: localStorage.getItem('token') || ''
+        }, */
+        success: function(res) {
+                if (res.status !== 0) { return layui.layer.msg('获取用户信息失败！') }
+                //调用renderAvatar函数渲染用户头像
+                renderAvatar(res.data)
+            }
+            // complete: function(res) {
+            //     if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            //         //1、强制清空 token
+            //         localStorage.removeItem('token')
+            //             //2、强制跳转到登录页面
+            //         location.href = '/login.html'
+            //     }
+            // }
+    })
+}
+// 渲染用户的头像
+function renderAvatar(user) {
+    // 1、获取用户名或昵称
+    var userName = user.nickname || user.username
+        // 2、设置欢迎的文本
+    $('#welcome').html('欢迎&nbsp;&nbsp;' + userName)
+        // 3、按需渲染用户的头像
+    if (user.user_pic !== null) {
+        // 3.1、 渲染图片头像
+        $('.layui-nav-img').attr('src', user.user_pic).show()
+        $('.text-avatar').hide()
+    } else {
+        // 3.2、 渲染文本头像
+        $('.layui-nav-img').hide()
+        var first = userName[0].toUpperCase()
+        $('.text-avatar').html(first).show()
+    }
+
+}
